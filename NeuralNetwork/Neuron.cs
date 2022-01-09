9815d800e1841b8e454b10f63b9faca1e82fade8
ZavNav <arka146@mail.ru>
2022-01-09 18:55:45 +0300

    Initial commit

---
+++ NeuralNetwork/Neuron.cs
@@ -0,0 +1,7 @@
+namespace NeuralNetwork
+{
+    public class Neuron
+    {
+        
+    }
+}
---
+++ NeuralNetwork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var firstNum = decimal.Parse(Console.ReadLine());
+            var secondNum = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+        }
+    }
+}
